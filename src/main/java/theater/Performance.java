add05be05cdf135166340e1d6b23863fc282ecf9
Zhengyu Yi <franco166@gmail.com>
2025-11-17 23:31:13 -0500

    Task 1.1: fix style issues and clean up code

--- src/main/java/theater/Performance.java
+++ src/main/java/theater/Performance.java
@@ -5,11 +5,38 @@ package theater;
  */
 public class Performance {
 
-    public String playID;
-    public int audience;
+    /** Identifier of the play that is being performed. */
+    private final String playID;
 
+    /** Number of audience members attending this performance. */
+    private final int audience;
+
+    /**
+     * Creates a performance.
+     *
+     * @param playID   identifier of the play
+     * @param audience audience size
+     */
     public Performance(String playID, int audience) {
         this.playID = playID;
         this.audience = audience;
+    }
+
+    /**
+     * Returns the identifier of the play.
+     *
+     * @return the play identifier
+     */
+    public String getPlayID() {
+        return playID;
+    }
+
+    /**
+     * Returns the audience size.
+     *
+     * @return the number of audience members
+     */
+    public int getAudience() {
+        return audience;
     }
 }
--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,46 @@
 package theater;
 
+/**
+ * Represents a play with a name and a type (for example tragedy or comedy).
+ *
+ * @author Zhengyu YI
+ * @version 1.0
+ */
 public class Play {
 
-    public String name;
-    public String type;
+    /** Human-readable name of the play. */
+    private final String name;
 
+    /** Type of the play, such as "tragedy" or "comedy". */
+    private final String type;
+
+    /**
+     * Creates a new play.
+     *
+     * @param name the title of the play
+     * @param type the type of the play
+     */
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
     }
+
+    /**
+     * Returns the name of this play.
+     *
+     * @return the play's name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Returns the type of this play.
+     *
+     * @return the play's type
+     */
+    public String getType() {
+        return type;
+    }
 }
+
